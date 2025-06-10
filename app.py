4a63b54805f92cdeb903c8bf78d4159104eef33b
yundogyun <axfgzx@gmail.com>
2025-06-10 16:45:46 +0900

    반복횟수 미적용 오류 해결

--- app.py
+++ app.py
@@ -9,25 +9,31 @@ def index():
 
 @app.route('/session')
 def session_setup():
-    return render_template('session.html', bgm='off', task='', goal='')
+    # session.html 폼을 띄울 때 기본값 지정
+    return render_template('session.html',
+                           bgm='off',
+                           task='',
+                           goal='')
 
 @app.route('/start', methods=['POST'])
 def start_session():
+    # form 에서 전달된 모든 값을 딕셔너리로 수집
     data = request.form.to_dict()
 
     # 기본값 처리
-    data['theme'] = '땅'  # 기본 테마
-    data['bgm'] = data.get('bgm', 'off')  # bgm 기본값 설정
-    data['workMinutes'] = int(data.get('workMinutes', 25))
+    data['theme']        = '땅'                       # 기본 테마
+    data['bgm']          = data.get('bgm', 'off')     # BGM 기본값
+    data['workMinutes']  = int(data.get('workMinutes', 25))
     data['breakMinutes'] = int(data.get('breakMinutes', 5))
-    data['repeatCount'] = int(data.get('repeatCount', 1))
-    data['task'] = data.get('task', '')
-    data['goal'] = data.get('goal', '')
+    # ★ 여기서 폼의 repeatCount 값을 받아 저장합니다.
+    data['repeatCount']  = int(data.get('repeatCount', 1))
+    data['task']         = data.get('task', '')
+    data['goal']         = data.get('goal', '')
 
-    # 작업 저장
+    # 데이터 저장 (로그나 기록용)
     recorder.save_task(data)
 
-    # 타이머 화면으로 전달
+    # timer.html 로 넘어갈 때 data 를 session 이라는 이름으로 넘겨줍니다.
     return render_template('timer.html', session=data)
 
 @app.route('/feedback')
